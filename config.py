0dbc7902b0d0986c5a53728b3a4dd56bcef53d3d
mustafaAlwindawi <55410521+mustafaAlwindawi@users.noreply.github.com>
2019-11-23 14:52:14 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+api_key = "wzUmqwrgXIaVisMOUrSXv3Hgd"
+api_secret = "fa9HgSTQahE3gw0UjUvydnmz2FzpgUkFhTC7RFB7W9YchTUyQb"
+access_token = "2654747796-MrlYijOP5CyqaEd1hzc25umNIH8H43WbHzwIsg5"
+token_secret = "SkyjIIJnr53GKRoL0bQYGbMXc8kxr2ueiJSpiQ06B7UbA"
